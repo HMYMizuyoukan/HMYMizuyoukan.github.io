94e592cf0e5b6d68e1d154fb920519893926472c
HMYMizuyoukan <104630267+HMYMizuyoukan@users.noreply.github.com>
2022-11-01 16:38:54 +0900

    Delete mail directory

--- mail/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "sanae2409@gmail.com";
-$title = "あなたのメールアドレスは危険です！";
-$message = "Amazonであなたのメールアドレスが不正に使用されていました。変更には100円かかります。";
-$header = "From: amazon.co.jp";
-
-mb_send_mail($to, $title, $message, $header);
